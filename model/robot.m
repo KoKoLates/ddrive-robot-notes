8e2a839140a8ad2d85e6415d88c281a850e2c85a
KoKoLates <the21515@gmail.com>
2023-10-31 00:39:24 +0800

    chore: update dd robote model

--- model/robot.m
+++ model/robot.m
@@ -2,20 +2,38 @@ classdef robot
     % A class definition for the differential drive robot
     
     properties
-        Property1
+        pose = [0, 0, 0]; % [x, y, theta]
+        wheel_diameter = 0.25;
+        track_width = 0.5;
+
+        % velocity attributes
+        velocity = 20;
+        wheel_rate = [
+            abs(sin(0: 0.01: 2 * pi)).*velocity; 
+            abs(cos(0: 0.01: 2 * pi)).*velocity;
+        ];
+        dt = 0.01; % time between iterations in seconds
     end
     
     methods
-        function obj = robot(inputArg1,inputArg2)
-            %ROBOT Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function [new_pose] = kinematics( ...
+                current_pose, linear_velocity, rotation_velocity, dt)
+            radius = abs(linear_velocity / rotation_velocity);
+
+            theta = current_pose(3);
+            % compute sin and cos at initial pose
+            s = sin(theta);
+            c = cos(theta);
+
+            % compute sin and cos at final pose
+            s_th = sin(theta + rotation_velocity * dt);
+            c_th = cos(theta + rotation_velocity * dt);
+
+
+            current_pose(1) = current_pose(1) + (linear_velocity * c_th) * dt;
+            % ...
+            
+            new_pose = current_pose;
         end
     end
 end
