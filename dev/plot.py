db6ad203c796c0a02f1be3e82a2c1e2b552731a5
KoKoLates <the21515@gmail.com>
2024-09-11 22:33:50 +0800

    chore(dev): update robot dimension for application plotting

--- dev/plot.py
+++ dev/plot.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from typing import Optional, Callable
 
+
 class Application(object):
     def __init__(
         self,
@@ -10,12 +11,12 @@ class Application(object):
         h: int,
         window_name: str,
         mouse_callback: Optional[Callable],
-        color: tuple[int, int, int] = (255, 255, 255)
+        color: tuple[int, int, int] = (255, 255, 255),
     ) -> None:
         self.w, self.h = w, h
         self.name: str = window_name
         self.color: tuple[int, int, int] = color
-        
+
         cv2.namedWindow(self.name)
         if mouse_callback is not None:
             cv2.setMouseCallback(self.name, mouse_callback)
@@ -24,15 +25,15 @@ class Application(object):
         self,
         points: np.ndarray,
         color: tuple[int, int, int] = (255, 0, 0),
-        thickness: int = 2
+        thickness: int = 2,
     ) -> None:
         for i in range(len(points) - 1):
             cv2.line(
                 self.canvas,
                 (points[i, 0], points[i, 1]),
-                (points[i+1, 0], points[i+1, 1]),
+                (points[i + 1, 0], points[i + 1, 1]),
                 color,
-                thickness
+                thickness,
             )
 
         cv2.line(
@@ -40,11 +41,11 @@ class Application(object):
             (points[0, 0], points[0, 1]),
             (points[-1, 0], points[-1, 1]),
             color,
-            thickness
+            thickness,
         )
 
     def clear(self) -> None:
-        self.canvas: np.ndarray = np.ones((self.h, self.w, 3)).astype('uint8')
+        self.canvas: np.ndarray = np.ones((self.h, self.w, 3)).astype("uint8")
         for i in range(len(self.color)):
             self.canvas[:, :, i] = self.canvas[:, :, i] * self.color[i]
 
@@ -53,63 +54,40 @@ class Application(object):
         points: list,
         color: tuple[int, int, int] = (255, 0, 0),
         thickness: int = 2,
-        dotted: bool = False
+        dotted: bool = False,
     ) -> None:
         for i in range(len(points) - 1):
             if dotted:
                 if not (i % 2):
                     cv2.circle(
-                        self.canvas,
-                        (points[i][0], points[i][1]),
-                        2,
-                        color,
-                        thickness
+                        self.canvas, (points[i][0], points[i][1]), 2, color, thickness
                     )
                 continue
-            
+
             cv2.line(
                 self.canvas,
                 (points[i][0], points[i][1]),
                 (points[i + 1][0], points[i + 1][1]),
                 color,
-                thickness
+                thickness,
             )
-            cv2.circle(
-                self.canvas,
-                (points[i][0], points[i][1]),
-                3, 
-                (0, 0, 0),
-                1
-            )
-        
+            cv2.circle(self.canvas, (points[i][0], points[i][1]), 3, (0, 0, 0), 1)
+
         if not dotted:
-            cv2.circle(
-                self.canvas,
-                (points[-1][0], points[-1][1]),
-                3,
-                (0, 0, 0),
-                1
-            )
+            cv2.circle(self.canvas, (points[-1][0], points[-1][1]), 3, (0, 0, 0), 1)
 
     def text(
         self,
         text: str,
         color: tuple[int, int, int] = (255, 0, 0),
         thickness: int = 2,
-        font_scale: int = 1, 
-        org: tuple[int, int] = (100, 50)
+        font_scale: int = 1,
+        org: tuple[int, int] = (100, 50),
     ) -> None:
         """adding text to the canvas"""
         font: int = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(
-            self.canvas,
-            text,
-            org,
-            font,
-            font_scale,
-            color,
-            thickness,
-            cv2.LINE_AA
+            self.canvas, text, org, font, font_scale, color, thickness, cv2.LINE_AA
         )
 
     def show(self) -> int:
